e81a180847d85d542f9271f7d9f68ba6a3a7a065
ThuanVu <thuan.vu.e@ctmcorp.com.vn>
2025-06-18 17:26:16 +0700

    Update types

---
+++ src/aframe-jsx.d.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    'a-scene': any;
+    'a-entity': any;
+    'a-box': any;
+    'a-sky': any;
+    'a-sphere': any;
+    'a-cylinder': any;
+    'a-camera': any;
+    'a-cursor': any;
+    'a-light': any;
+    'a-assets': any;
+    'a-text': any;
+  }
+}
