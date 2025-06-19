ecc28aaaa5f1dd3c18fdb959dab6f7c69cdcb14d
ThuanVu <thuan.vu.e@ctmcorp.com.vn>
2025-06-19 13:10:19 +0700

    Add Phôt

--- src/app/data/stations/Start.tsx
+++ src/app/data/stations/Start.tsx
@@ -4,7 +4,7 @@ const StartStation: Station = {
     code: 'start',
     name: 'start',
     description: 'start point',
-    backgroundUrl: '/station/IH002BG.jpg',
+    backgroundUrl: '/station/start.jpg',
     latitude: 7,
     longitude: 0,
 };
